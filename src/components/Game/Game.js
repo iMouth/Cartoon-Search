e78ee443454e5b026e45956d1a2d3a55f3a87fd1
Kelvin Etori <kelvinkamau1130@yahoo.com>
2022-08-08 18:27:05 -0700

    added character chooses on map click

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
+import circle from "./assets/select.svg";
 import "./Game.css";
 
 const Game = ({ mapInfo }) => {
   function imgClick(e) {
-    console.log(e);
+    const ele = document.getElementById("picker");
+    ele.style.display = "block";
+    const newLeft = window.innerWidth - 150 - e.pageX < ele.offsetWidth ? e.pageX - ele.offsetWidth - 50 : e.pageX + 50;
+    const newTop = window.innerHeight - e.pageY < ele.offsetHeight ? e.pageY - ele.offsetHeight : e.pageY;
+    ele.style.left = newLeft + "px";
+    ele.style.top = newTop + "px";
+
+    const circle = document.getElementById("picker-circle");
+    circle.style.left = e.pageX + "px";
+    circle.style.top = e.pageY + "px";
+    circle.style.transform = "translate(-50px, -50px)";
+    circle.style.display = "block";
   }
+
+  function closePicker() {
+    document.getElementById("picker").style.display = "none";
+    document.getElementById("picker-circle").style.display = "none";
+  }
+
+  window.addEventListener("resize", closePicker);
 
   let { map } = useParams();
   let mapImg = null;
@@ -25,6 +44,12 @@ const Game = ({ mapInfo }) => {
         <div className="characters">{charList}</div>
       </div>
       <img className={map} src={mapImg} alt="" onClick={(e) => imgClick(e)} />
+      <div id="picker">
+        <div className="picker-holder">{charList}</div>
+      </div>
+      <div id="picker-circle">
+        <img src={circle} alt="" />
+      </div>
     </div>
   );
 };
