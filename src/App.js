f947eb5362af580b63171bb60d8b40d677cbb804
Kelvin Etori <kelvinkamau1130@yahoo.com>
2022-08-12 09:40:52 -0700

    fixed leaderboard css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
 import Home from "./components/Home/Home";
 import Game from "./components/Game/Game";
 import Leaderboard from "./components/Leaderboard/Leaderboard";
